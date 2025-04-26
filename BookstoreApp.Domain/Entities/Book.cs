230c078cf93dd1878b4f7498035d797e03f7f3c8
kkhashayar <frankdezz@gmail.com>
2025-04-26 11:12:23 +0200

    Init commit

---
+++ BookstoreApp.Domain/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookstoreApp.Domain.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? ISBN { get; set; }
+    public int AuthoId { get; set; }
+    public int CategoryId { get; set; }
+    public decimal? Price { get; set; }
+    public int StockQuantity { get; set; }
+}
---
+++ BookstoreApp.Infrastructure/BookstoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using BookstoreApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BookstoreApp.Infrastructure;
+
+public class BookstoreDbContext : DbContext
+{
+    public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Book> Books { get; set; }  
+}
