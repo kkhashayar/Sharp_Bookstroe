43b90df99c0a51a271f335020ff959a6e1761cf0
kkhashayar <frankdezz@gmail.com>
2025-04-26 11:31:45 +0200

    install packages ef core, tools and design, adjust properties in entities.

--- BookstoreApp.Domain/Entities/Book.cs
+++ BookstoreApp.Domain/Entities/Book.cs
@@ -3,7 +3,7 @@
 public class Book
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public string? ISBN { get; set; }
     public int AuthoId { get; set; }
     public int CategoryId { get; set; }
