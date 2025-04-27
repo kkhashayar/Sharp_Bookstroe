397f056da26c6f1f829d5a6a58d77b9a8859a9b7
kkhashayar <frankdezz@gmail.com>
2025-04-27 08:56:06 +0200

    Added migrations and update database for new guid id in author and book

--- BookstoreApp.Infrastructure/BookstoreDbContext.cs
+++ BookstoreApp.Infrastructure/BookstoreDbContext.cs
@@ -14,3 +14,11 @@ public class BookstoreDbContext : DbContext
 
     public DbSet<Book> Books { get; set; }  
 }
+
+/*
+ * 
+ * 
+   dotnet ef migrations add GuidId_In_Book_And_Author --project ../BookstoreApp.Infrastructure --startup-project .
+   dotnet ef database update --project ../BookstoreApp.Infrastructure --startup-project .
+ * 
+ */
---
+++ BookstoreApp.Infrastructure/Migrations/20250427065418_GuidId_In_Book_And_Author.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookstoreApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuidId_In_Book_And_Author : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BookId",
+                table: "Books",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Books");
+        }
+    }
+}
