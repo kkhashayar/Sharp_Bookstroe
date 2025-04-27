293ae67f684d083356e49955eb930eefa8823432
kkhashayar <frankdezz@gmail.com>
2025-04-27 16:49:15 +0200

    Added migration

---
+++ BookstoreApp.Infrastructure/Migrations/20250427144403_Seed_And_Relation.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookstoreApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed_And_Relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Authors");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors",
+                column: "AuthorId");
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "AuthorId", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), "George Orwell" },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), "J.K. Rowling" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "AuthorName", "BookId", "CategoryId", "ISBN", "Price", "StockQuantity", "Title" },
+                values: new object[,]
+                {
+                    { 1, new Guid("11111111-1111-1111-1111-111111111111"), "George Orwell", new Guid("33333333-3333-3333-3333-333333333333"), 1, "9780451524935", 15, 100, "1984" },
+                    { 2, new Guid("22222222-2222-2222-2222-222222222222"), "J.K. Rowling", new Guid("44444444-4444-4444-4444-444444444444"), 2, "9780747532699", 20, 50, "Harry Potter and the Philosopher's Stone" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors");
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "AuthorId",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "AuthorId",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Authors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors",
+                column: "Id");
+        }
+    }
+}
