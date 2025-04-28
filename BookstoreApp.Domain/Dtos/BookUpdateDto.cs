523f6b19a80d29c6afabf8004836dc61c4f4d62c
kkhashayar <frankdezz@gmail.com>
2025-04-28 14:47:47 +0200

    Added BookUpdateDto

--- BookstoreApp.Domain/Dtos/BookUpdateDto.cs
+++ BookstoreApp.Domain/Dtos/BookUpdateDto.cs
@@ -4,4 +4,10 @@
 public class BookUpdateDto
 {
     public string Title { get; set; }
+    public string? ISBN { get; set; }
+    public Guid AuthorId { get; set; }
+    public string Category { get; set; }
+    public int? Price { get; set; }
+    public int StockQuantity { get; set; }
+
 }
