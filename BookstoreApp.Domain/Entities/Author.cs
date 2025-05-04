4434025a86ff92ca37d92a499306d270e2c3eb8f
kkhashayar <frankdezz@gmail.com>
2025-05-04 11:51:52 +0200

    Added Auhor info and Book.Description, added migrations and updated database

--- BookstoreApp.Domain/Entities/Author.cs
+++ BookstoreApp.Domain/Entities/Author.cs
@@ -1,8 +1,12 @@
-﻿namespace BookstoreApp.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace BookstoreApp.Domain.Entities;
 
 public class Author
 {
     public Guid AuthorId { get; set; }
     public string? Name { get; set; }
-    public ICollection<Book> Books { get; set; } = new List<Book>();    
+    public ICollection<Book> Books { get; set; } = new List<Book>();
+
+    public string? ShortInfo { get; set; }
 }
--- BookstoreApp.Domain/Entities/Book.cs
+++ BookstoreApp.Domain/Entities/Book.cs
@@ -12,6 +12,7 @@ public class Book
     public int CategoryId { get; set; }
     public int? Price { get; set; }
     public int StockQuantity { get; set; }
-    public Author? Author { get; set; }  
+    public Author? Author { get; set; }
+    public string? Description { get; set; }
 
 }
